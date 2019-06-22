1915e2f4b0d95894f3cb3615a41ce61593008919
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-21 20:54:20 -0600

    add card and styles

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Card = props => {
+  return (
+    <div className="card">
+      <img src={`${props.picture}`}></img>
+    </div>
+  )
+}
+
+export default Card
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,9 +1,17 @@
 import React from "react"
+import project1 from "../../static/project-screenshots/usacountyexplorer.com.png"
+import project2 from "../../static/project-screenshots/lyricslog-home-view.png"
 
 const Portfolio = () => {
   return (
     <section className="portfolio">
-      <h1>portfolio view goes here</h1>
+      <h1>Projects</h1>
+      <div className="card">
+        <img src={project1}></img>
+      </div>
+      <div className="card">
+        <img src={project2}></img>
+      </div>
     </section>
   )
 }
