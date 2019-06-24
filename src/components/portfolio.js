0fff1f43bee1d85eff9624252ee5248c347d2362
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-23 20:53:37 -0600

    update portfolio view

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,16 +1,60 @@
-import React from "react"
+import React, { useState } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import project1 from "../../static/project-screenshots/usacountyexplorer.com.png"
 import project2 from "../../static/project-screenshots/lyricslog-home-view.png"
 
 const Portfolio = () => {
+  const [show, setShow] = useState(`none`)
+
   return (
     <section className="portfolio">
-      <h1>Projects</h1>
-      <div className="card">
-        <img src={project1}></img>
+      <div
+        className="modal-container"
+        style={{
+          display: `${show}`,
+        }}
+      >
+        <div className="modal">
+          <div className="slider-container"></div>
+          <div className="slider-description">
+            <div>
+              <h3>USA County Explorer</h3>
+            </div>
+            <div>
+              <p>
+                Site for people to explore US counties and visualize where they
+                might enjoy living. Counties can be clicked to display
+                information, and the sliders can be adjusted or toggled to
+                re-shade the map according to the user's desires. Dark counties
+                most closely represent the user's interests, and light counties
+                least represent them.
+              </p>
+            </div>
+            <div className="slider-description-bottom">
+              <button>
+                <a href="https://usacountyexplorer.com/">View Live Site</a>
+              </button>
+              <FontAwesomeIcon
+                icon={faArrowRight}
+                onClick={() => setShow(`none`)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card">
-        <img src={project2}></img>
+      <h1>Projects</h1>
+      <div className="card-container">
+        <div className="card">
+          <img src={project1} alt=""></img>
+        </div>
+        <button onClick={() => setShow(`flex`)}>View More</button>
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <img src={project2} alt=""></img>
+        </div>
+        <button>View More</button>
       </div>
     </section>
   )
