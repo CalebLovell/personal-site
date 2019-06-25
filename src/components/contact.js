9bb77a51e1a1b1a00fda0eb09ef887e4a1df2c79
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 17:06:25 -0600

    add contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,6 +4,15 @@ const Contact = () => {
   return (
     <section className="contact">
       <h1>contact view goes here</h1>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input name="name" placeholder="Your Name" type="text"></input>
+        <button>Send</button>
+      </form>
     </section>
   )
 }
