4dfa2d9ac12b967eb236e38a38b0fbf3bfb4ce7e
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 17:39:21 -0600

    update forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,11 +3,16 @@ import React from "react"
 const Contact = () => {
   return (
     <section className="contact">
-      <form name="contact" method="post" data-netlify="true" data-netlify-recaptcha="true" >
-        <input type="text" name="name" placeholder="Name"></input>
-        <input type="text" name="email" placeholder="Email"></input>
-        <textarea name="message" placeholder="Message" rows="7"></textarea>
-        <div data-netlify-recaptcha="true"></div>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-recaptcha="true"
+      >
+        <input type="text" name="name" placeholder="Name" />
+        <input type="text" name="email" placeholder="Email" />
+        <textarea name="message" placeholder="Message" rows="7" />
+        <div data-netlify-recaptcha="true" />
         <button>Send</button>
       </form>
     </section>
