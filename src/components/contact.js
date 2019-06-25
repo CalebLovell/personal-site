997eb4af797622742b95c2b6d23f737cfd9a2b39
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 17:45:09 -0600

    add hidden input

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,13 +4,14 @@ const Contact = () => {
   return (
     <section className="contact">
       <form
-        name="contact"
+        name="Contact"
         method="post"
         data-netlify="true"
         data-netlify-recaptcha="true"
       >
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" placeholder="Name" />
-        <input type="text" name="email" placeholder="Email" />
+        <input type="email" name="email" placeholder="Email" />
         <textarea name="message" placeholder="Message" rows="7" />
         <div data-netlify-recaptcha="true" />
         <button>Send</button>
