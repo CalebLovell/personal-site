26f7606527e080ab4ad4510bee5a051994544bc2
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 18:40:08 -0600

    fix form bug

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,14 +4,14 @@ const Contact = () => {
   return (
     <section className="contact">
       <form
-        name="Contact"
+        name="contact"
         method="post"
         data-netlify="true"
         data-netlify-recaptcha="true"
       >
-        {/* <input type="hidden" name="formname" value="contact" /> */}
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="email" />
+        <input type="email" name="email" placeholder="E-mail" />
         <textarea name="message" placeholder="Message" rows="7" />
         <div data-netlify-recaptcha="true" />
         <button>Send</button>
