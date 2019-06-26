2a4265d80c0334b10ce940d1f2d727fdb1d151b1
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 19:54:09 -0600

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { Component, createRef } from "react"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
- 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+
 // Vars for editing circle properties
 const colors = [
   `rgba(237,235,246,`,
@@ -23,8 +23,8 @@ class Home extends Component {
         x: undefined,
         y: undefined,
       },
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight,
+      windowWidth: 0,
+      windowHeight: 0,
     }
   }
 
@@ -57,11 +57,17 @@ class Home extends Component {
       update: function() {
         // Updates our circles in various ways when invoked
         // Bounce circles off x-axis walls
-        if (this.x + this.radius > self.canvasRef.current.width || this.x - this.radius < 0) {
+        if (
+          this.x + this.radius > self.canvasRef.current.width ||
+          this.x - this.radius < 0
+        ) {
           this.dx = -this.dx
         }
         // Bounce circles off y-axis walls
-        if (this.y + this.radius > self.canvasRef.current.height || this.y - this.radius < 0) {
+        if (
+          this.y + this.radius > self.canvasRef.current.height ||
+          this.y - this.radius < 0
+        ) {
           this.dy = -this.dy
         }
 
@@ -71,19 +77,34 @@ class Home extends Component {
 
         // Interactivity; Updates circles based on the mouseArea
         let mouseArea = 75
-        if (self.state.mousePosition.x - this.x < mouseArea && self.state.mousePosition.x - this.x > -mouseArea &&
-            self.state.mousePosition.y - this.y < mouseArea && self.state.mousePosition.y - this.y > -mouseArea
+        if (
+          self.state.mousePosition.x - this.x < mouseArea &&
+          self.state.mousePosition.x - this.x > -mouseArea &&
+          self.state.mousePosition.y - this.y < mouseArea &&
+          self.state.mousePosition.y - this.y > -mouseArea
         ) {
           // Circle Growth
           if (this.radius < zoomedRadius) {
             let innerGrowth = 5 // 1st statement: Grows all circles central enough to expand quickly
             let outerGrowth = 1 // 2nd statement: Grows all circles not central enough to expand quickly
-            if (this.x + zoomedRadius + innerGrowth < self.canvasRef.current.width && this.y + zoomedRadius + innerGrowth < self.canvasRef.current.height &&
-              !(this.x - zoomedRadius - innerGrowth < 0) && !(this.y - zoomedRadius - innerGrowth < 0)) {
-                this.radius += innerGrowth // 1st Statement
-            } else if (this.x + this.radius + outerGrowth < self.canvasRef.current.width && this.y + this.radius + outerGrowth < self.canvasRef.current.height &&
-                     !(this.x - this.radius - outerGrowth < 0) && !(this.y - this.radius - outerGrowth < 0)) {
-                       this.radius += outerGrowth // 2nd Statement
+            if (
+              this.x + zoomedRadius + innerGrowth <
+                self.canvasRef.current.width &&
+              this.y + zoomedRadius + innerGrowth <
+                self.canvasRef.current.height &&
+              !(this.x - zoomedRadius - innerGrowth < 0) &&
+              !(this.y - zoomedRadius - innerGrowth < 0)
+            ) {
+              this.radius += innerGrowth // 1st Statement
+            } else if (
+              this.x + this.radius + outerGrowth <
+                self.canvasRef.current.width &&
+              this.y + this.radius + outerGrowth <
+                self.canvasRef.current.height &&
+              !(this.x - this.radius - outerGrowth < 0) &&
+              !(this.y - this.radius - outerGrowth < 0)
+            ) {
+              this.radius += outerGrowth // 2nd Statement
             }
           }
           // Circle Shrinkage
@@ -104,20 +125,29 @@ class Home extends Component {
     for (let i = 0; i < numberOfCircles; i++) {
       let radius = Math.random() * 2 + 3
       let zoomedRadius = radius * 3
-      let x = Math.random() * (this.canvasRef.current.width - radius * 2) + radius // Spawns circle anywhere along x-axis canvas minus the circle size
-      let y = Math.random() * (this.canvasRef.current.height - radius * 2) + radius // Spawns circle anywhere along y-axis canvas minus the circle size
+      let x =
+        Math.random() * (this.canvasRef.current.width - radius * 2) + radius // Spawns circle anywhere along x-axis canvas minus the circle size
+      let y =
+        Math.random() * (this.canvasRef.current.height - radius * 2) + radius // Spawns circle anywhere along y-axis canvas minus the circle size
       let dx = (Math.random() - 0.5) * 1 // Sets a starting velocity in either left or right direction
       let dy = (Math.random() - 0.5) * 1 // Sets a starting velocity in either up or down direction
       let color = colors[Math.floor(Math.random() * colors.length)]
       let opacity = Math.random()
-      circleArray.push(this.makeCircles(radius, x, y, dx, dy, color, opacity, zoomedRadius))
+      circleArray.push(
+        this.makeCircles(radius, x, y, dx, dy, color, opacity, zoomedRadius)
+      )
     }
   }
 
   animate = () => {
     const c = this.canvasRef.current.getContext("2d")
     requestAnimationFrame(this.animate) // Creates loop with callback function 'animate'
-    c.clearRect(0, 0, this.canvasRef.current.width, this.canvasRef.current.height) // Clears canvas when called
+    c.clearRect(
+      0,
+      0,
+      this.canvasRef.current.width,
+      this.canvasRef.current.height
+    ) // Clears canvas when called
     for (let i = 0; i < circleArray.length; i++) {
       // Calls update method of Circle on every circle when animate is called
       circleArray[i].update()
@@ -133,6 +163,7 @@ class Home extends Component {
   }
 
   componentDidMount() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight })
     this.init()
     requestAnimationFrame(this.animate)
     window.addEventListener("resize", this.handleResize)
@@ -161,9 +192,13 @@ class Home extends Component {
           />
         </div>
         <div className="intro-container">
-          <h1>Hello! I'm <span>Caleb Lovell</span>.</h1>
+          <h1>
+            Hello! I'm <span>Caleb Lovell</span>.
+          </h1>
           <h1>I'm a web developer.</h1>
-          <button>View my work  <FontAwesomeIcon icon={faArrowRight} /></button>
+          <button>
+            View my work <FontAwesomeIcon icon={faArrowRight} />
+          </button>
         </div>
       </section>
     )
