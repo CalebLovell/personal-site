e52d56ed1d77a4bf820e9863b8c406e592ab5e0f
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-06-25 18:45:50 -0600

    add honeypot

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,11 +7,16 @@ const Contact = () => {
         name="contact"
         method="post"
         data-netlify="true"
-        data-netlify-recaptcha="true"
+        netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact" />
+        <input
+          type="hidden"
+          name="bot-field"
+          value="Don't fill this out if you're a human "
+        />
         <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="E-mail" />
+        <input type="email" name="email" placeholder="Email" />
         <textarea name="message" placeholder="Message" rows="7" />
         <div data-netlify-recaptcha="true" />
         <button>Send</button>
