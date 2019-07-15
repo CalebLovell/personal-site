97827c21e3e69eabbd3a9249691816db826f4c5d
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-14 18:52:12 -0600

    add state to make it work? pretty please?

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Contact = () => {
+  const [name, setName] = useState(``)
+  const [email, setEmail] = useState(``)
+  const [message, setMessage] = useState(``)
+
+  const handleChangeName = e => {
+    setName(e.target.value)
+  }
+
+  const handleChangeEmail = e => {
+    setEmail(e.target.value)
+  }
+
+  const handleChangeMessage = e => {
+    setMessage(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    fetch("/", {
+      method: "POST",
+      body: message
+      })
+      .then(() => alert(`success`))
+      .catch(error => alert(error))
+  }
+
   return (
     <section className="contact" id="contact">
       <h1 className="title">Contact</h1>
@@ -10,7 +36,7 @@ const Contact = () => {
         method="post"
         data-netlify="true"
         netlify-honeypot="bot-field"
-        onsubmit="return false"
+        onSubmit={handleSubmit}
       >
         <input type="hidden" name="form-name" value="contact" />
         <input
@@ -18,9 +44,23 @@ const Contact = () => {
           name="bot-field"
           value="Don't fill this out if you're a human."
         />
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="Email" />
-        <textarea name="message" placeholder="Message" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChangeName}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChangeEmail}
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          onChange={handleChangeMessage}
+        />
         <div>
           <button type="submit">SEND</button>
         </div>
