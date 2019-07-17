829e7a775577249356fe22438ced287225cd1612
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-17 17:47:11 -0600

    Update favicon, add resume download

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Bars from "./bars"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons"
+
 import profPic from "../../static/prof-pic.jpg"
+import resume from "../../static/resume.pdf"
 
 const About = () => {
   return (
@@ -14,12 +18,19 @@ const About = () => {
               <img src={profPic} alt="my face" />
             </div>
           </div>
-          <h2 className="profile-subtitle">Who is this guy?</h2>
+          <h2 className="profile-subtitle">
+            Who is this guy? &nbsp;
+            <a href={`${resume}`} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFileDownload} title="Download Resume" />
+            </a>
+          </h2>
           <div className="profile-description">
             <p>
-              I'm a full-stack <span>web developer</span> with a particular passion for
-              making interactive front-end projects and data visualizations. I
-              am a voracious learner and love to teach myself new things. In my free time you can find me reading, discussing politics or enjoying the ourdoors!
+              I'm a full-stack <span>web developer</span> with a particular
+              passion for making interactive front-end projects and data
+              visualizations. I am a voracious learner and love to teach myself
+              new things. In my free time you can find me reading, discussing
+              politics or enjoying the ourdoors!
             </p>
           </div>
         </div>
