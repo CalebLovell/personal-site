28208ff82c9dc8f2e4cfea4643561311ee49f822
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-18 10:46:44 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
+import sitePreview from "../../static/portfolio-screenshot.png"
 
 const IndexPage = () => {
   return (
     <>
       <Helmet>
         <title>Caleb Lovell's Developer Portfolio</title>
+        <meta
+          property="og:title"
+          content="Caleb Lovell's Developer Portfolio"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.caleblovell.dev/" />
+        <meta property="og:image" content="sitePreview" />
+        <meta
+          property="og:description"
+          description="Hello! I'm Caleb Lovell. I'm a full-stack web developer."
+        />
       </Helmet>
       <Layout />
     </>
