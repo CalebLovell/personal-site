0b49d1518d271f39e7354b0034d0c7dd9dda4920
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-18 10:41:21 -0600

    add react helmet and open graph protocols

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <Layout />
+    <>
+      <Helmet>
+        <title>Caleb Lovell's Developer Portfolio</title>
+      </Helmet>
+      <Layout />
+    </>
   )
 }
 
