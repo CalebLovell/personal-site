8acc183d94552d8929785c1f5615fb871eb783b1
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-07-18 11:02:32 -0600

    move meta info to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  siteMetadata: {
+    title: "Caleb Lovell's Developer Portfolio",
+    titleTemplate: "%s · The Real Hero",
+    description: "Hello! I'm Caleb Lovell. I'm a full-stack web developer.",
+    url: "https://caleblovell.dev",
+    image: "/portfolio-screenshot.png",
+    type: "website",
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,6 @@ const IndexPage = () => {
     <>
       <Helmet>
         <title>Caleb Lovell's Developer Portfolio</title>
-        <meta
-          property="og:title"
-          content="Caleb Lovell's Developer Portfolio"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://www.caleblovell.dev/" />
-        <meta
-          property="og:image"
-          content="https://www.caleblovell.dev/static/portfolio-screenshot-030b4ebec81345d8baddd5de18589956.png"
-        />
-        <meta
-          property="og:description"
-          description="Hello! I'm Caleb Lovell. I'm a full-stack web developer."
-        />
       </Helmet>
       <Layout />
     </>
