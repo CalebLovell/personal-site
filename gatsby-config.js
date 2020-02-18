1e7a7585fe2157ddaa3fdb3d4e888614bec86a31
Caleb Lovell <caleblovell1@gmail.com>
2020-02-18 02:14:13 -0700

    try new image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
-  siteMetadata: {
-    type: "website",
-    title: "Caleb Lovell's Developer Portfolio",
-    url: "https://caleblovell.dev",
-    image: "/portfolio-screenshot.png",
-    description: "Hello! I'm Caleb Lovell. I'm a full-stack web developer.",
-  },
-}
+	plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+	siteMetadata: {
+		type: `website`,
+		title: `Caleb Lovell's Developer Portfolio`,
+		url: `https://caleblovell.dev`,
+		image: `./static/portfolio-screenshot.png`,
+		description: `Hello! I'm Caleb Lovell. I'm a full-stack web developer.`,
+	},
+};
